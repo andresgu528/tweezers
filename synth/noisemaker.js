02551259409aa4338a8eaa939c93df4f09e22dfd
Andres Guarin <anguco528@gmail.com>
2025-09-25 15:24:58 +0200

    Fix synth_processor not properly unified into noisemaker module

--- synth/noisemaker.js
+++ synth/noisemaker.js
@@ -12,7 +12,7 @@ export class NoiseMaker {
   }
   async setUserFunction(userFunction) {
     this.userFunction = userFunction;
-    await this.audioContext.audioWorklet.addModule("synth_processor.js");
+    await this.audioContext.audioWorklet.addModule(synthProcessorURL);
     this.audioWorkletNode = new AudioWorkletNode(
         this.audioContext,
         "synth_processor",
@@ -30,3 +30,46 @@ export class NoiseMaker {
     this.audioWorkletNode.port.postMessage(params);
   }
 };
+
+let synthProcessorCode = `
+  class SynthProcessor extends AudioWorkletProcessor {
+    constructor(options) {
+      super();
+      this.count = 0;
+      this.sampleRate = options.processorOptions.sampleRate;
+      this.paramNames = options.processorOptions.userFunction.paramNames;
+      this.paramValues = options.processorOptions.userFunction.paramValues;
+      this.userFunction = new Function(
+        options.processorOptions.userFunction.arg,
+        ...this.paramNames,
+        options.processorOptions.userFunction.func
+      );
+      this.tdelta = options.processorOptions.tdelta;
+      
+      this.port.onmessage = (event) => {
+        this.paramValues = event.data;
+      };
+      this.time = 0;
+    }
+
+    process(inputs, outputs, params) {
+      const data = outputs[0][0];
+      for (let i = 0; i < data.length; i++) {
+        data[i] = this.userFunction(this.time, ...this.paramValues);
+        this.time += this.tdelta;
+      }
+      this.count += 1;
+      if (this.count > this.sampleRate/data.length/2) {
+        this.count = 0;
+      }
+      return true;
+    }
+  }
+
+  registerProcessor("synth_processor", SynthProcessor);
+`
+let synthProcessorModule = new Blob(
+  [synthProcessorCode],
+  {type: "text/javascript"}
+);
+let synthProcessorURL = URL.createObjectURL(synthProcessorModule);
